237a4269f3716a7e6b34f36124fceb2c60739c2d
Tyler Wysocki <115134475+tywysocki@users.noreply.github.com>
2023-03-23 07:07:43 -0600

    add image generation request, update chat request.

--- Sources/OpenAI/OpenAI.swift
+++ Sources/OpenAI/OpenAI.swift
@@ -11,9 +11,23 @@ public enum OpenAIError: Error {
 
 public class OpenAI {
     fileprivate(set) var token: String?
+    fileprivate let config: Config
+    
+        /// Configuration object for the client
+    public struct Config {
+        
+        /// Initialiser
+        /// - Parameter session: the session to use for network requests.
+        public init(session: URLSession = URLSession.shared) {
+            self.session = session
+        }
+
+        let session:URLSession
+    }
     
     public init(authToken: String) {
         self.token = authToken
+        self.config = Config()
     }
 }
 
@@ -24,16 +38,16 @@ extension OpenAI {
     ///   - model: Defaults to `OpenAIModelType.gpt3(.davinci)` 
     ///   - maxTokens: Set word limit for the response
     ///   - completionHandler: Returns an OpenAI Data Model
-    public func sendCompletion(with prompt: String, model: OpenAIModelType = .gpt3(.davinci), maxTokens: Int = 16, completionHandler: @escaping (Result<OpenAIModel, OpenAIError>) -> Void) {
+    public func sendCompletion(with prompt: String, model: OpenAIModelType = .gpt3(.davinci), maxTokens: Int = 16, temperature: Double = 1, completionHandler: @escaping (Result<OpenAIModel, OpenAIError>) -> Void) {
         let endpoint = Endpoint.completions
-        let body = Command(prompt: prompt, model: model.modelName, maxTokens: maxTokens)
+        let body = Command(prompt: prompt, model: model.modelName, maxTokens: maxTokens, temperature: temperature)
         let request = prepareRequest(endpoint, body: body)
         
         makeRequest(request: request) { result in
             switch result {
             case .success(let success):
                 do {
-                    let res = try JSONDecoder().decode(OpenAIModel.self, from: success)
+                    let res = try JSONDecoder().decode(OpenAIModel<TextResult>.self, from: success)
                     completionHandler(.success(res))
                 } catch {
                     completionHandler(.failure(.decodingError(error: error)))
@@ -50,7 +64,7 @@ extension OpenAI {
     ///   - model: The only support model is `text-davinci-edit-001`
     ///   - input: Input Example "berds can fly"
     ///   - completionHandler: Returns an OpenAI Data Model
-    public func sendEdits(with instruction: String, model: OpenAIModelType = .feature(.davinci), input: String = "", completionHandler: @escaping (Result<OpenAIModel, OpenAIError>) -> Void) {
+    public func sendEdits(with instruction: String, model: OpenAIModelType = .feature(.davinci), input: String = "", completionHandler: @escaping (Result<OpenAIModel<TextResult>, OpenAIError>) -> Void) {
         let endpoint = Endpoint.edits
         let body = Instruction(instruction: instruction, model: model.modelName, input: input)
         let request = prepareRequest(endpoint, body: body)
@@ -59,7 +73,7 @@ extension OpenAI {
             switch result {
             case .success(let success):
                 do {
-                    let res = try JSONDecoder().decode(OpenAIModel.self, from: success)
+                    let res = try JSONDecoder().decode(OpenAIModel<TextResult>.self, from: success)
                     completionHandler(.success(res))
                 } catch {
                     completionHandler(.failure(.decodingError(error: error)))
@@ -74,19 +88,48 @@ extension OpenAI {
     /// - Parameters:
     ///   - messages: Array of `ChatMessages`
     ///   - model: The only support model is `gpt-3.5-turbo`
-    ///   - maxTokens: Sets the maximum number of words allowed in the response.
-    ///   - temperature: Adjusts the randomness of the output, which increases from 0 to 1
+    ///   - user: An Identifier that represents your end-user (can help OpenAI monitor and detect abuse).
+    ///   - temperature: Set between 0 and 2. Higher values will make the output more random, while lower values will make it more focused and deterministic.
+    ///   - topProbabilityMass: The OpenAI api equivalent of the "top_p" parameter. An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or temperature but not both.
+    ///   - choices: How many chat completion choices to generate for each input message.
+    ///   - stop: Up to 4 sequences where the API will stop generating further tokens.
+    ///   - maxTokens: The maximum number of tokens allowed for the generated answer. By default, the number of tokens the model can return will be (4096 - prompt tokens).
+    ///   - presencePenalty: Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    ///   - frequencyPenalty: Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    ///   - logitBias: Modify the likelihood of specified tokens appearing in the completion. Maps tokens (specified by their token ID in the OpenAI Tokenizer—not English words) to an associated bias value from -100 to 100. Values between -1 and 1 should decrease or increase likelihood of selection; values like -100 or 100 should result in a ban or exclusive selection of the relevant token.
     ///   - completionHandler: Returns an OpenAI Data Model
-    public func sendChat(with messages: [ChatMessage], model: OpenAIModelType = .chat(.chatgpt), maxTokens: Int? = nil, temperature: Double = 1.0, completionHandler: @escaping (Result<OpenAI<MessageResult>, OpenAIError>) -> Void) {
+    public func sendChat(with messages: [ChatMessage],
+                         model: OpenAIModelType = .chat(.chatgpt),
+                         user: String? = nil,
+                         temperature: Double? = 1,
+                         topProbabilityMass: Double? = 0,
+                         choices: Int? = 1,
+                         stop: [String]? = nil,
+                         maxTokens: Int? = nil,
+                         presencePenalty: Double? = 0,
+                         frequencyPenalty: Double? = 0,
+                         logitBias: [Int: Double]? = nil,
+                         completionHandler: @escaping (Result<OpenAIModel<MessageResult>, OpenAIError>) -> Void) {
         let endpoint = Endpoint.chat
-        let body = ChatConversation(messages: messages, model: model.modelName, maxTokens: maxTokens, temperature: temperature)
+        let body = ChatConversation(user: user,
+                                    messages: messages,
+                                    model: model.modelName,
+                                    temperature: temperature,
+                                    topProbabilityMass: topProbabilityMass,
+                                    choices: choices,
+                                    stop: stop,
+                                    maxTokens: maxTokens,
+                                    presencePenalty: presencePenalty,
+                                    frequencyPenalty: frequencyPenalty,
+                                    logitBias: logitBias)
+
         let request = prepareRequest(endpoint, body: body)
         
         makeRequest(request: request) { result in
             switch result {
                 case .success(let success):
                     do {
-                        let res = try JSONDecoder().decode(OpenAI<MessageResult>.self, from: success)
+                        let res = try JSONDecoder().decode(OpenAIModel<MessageResult>.self, from: success)
                         completionHandler(.success(res))
                     } catch {
                         completionHandler(.failure(.decodingError(error: error)))
@@ -97,8 +140,35 @@ extension OpenAI {
         }
     }
     
+    /// Send a Image generation request to the OpenAI API
+    /// - Parameters:
+    ///   - prompt: Text Prompt
+    ///   - numImages: The number of images to generate, default = 1
+    ///   - size: Size of the image, defaults to 1024x1024. Other options: 512x512 and 256x256
+    ///   - user: Optional identifier that represents your end-user (can help OpenAI to monitor and detect abuse).
+    ///   - completionHandler: Returns an OpenAI Data Model
+    public func sendImages(with prompt: String, numImages: Int = 1, size: ImageSize = .size1024, user: String? = nil, completionHandler: @escaping (Result<OpenAIModel<UrlResult>, OpenAIError>) -> Void) {
+        let endpoint = Endpoint.images
+        let body = ImageGeneration(prompt: prompt, n: numImages, size: size, user: user)
+        let request = prepareRequest(endpoint, body: body)
+
+        makeRequest(request: request) { result in
+            switch result {
+                case .success(let success):
+                    do {
+                        let res = try JSONDecoder().decode(OpenAIModel<UrlResult>.self, from: success)
+                        completionHandler(.success(res))
+                    } catch {
+                        completionHandler(.failure(.decodingError(error: error)))
+                    }
+                case .failure(let failure):
+                    completionHandler(.failure(.genericError(error: failure)))
+                }
+        }
+    }
+    
     private func makeRequest(request: URLRequest, completionHandler: @escaping (Result<Data, Error>) -> Void) {
-        let session = URLSession.shared
+        let session = config.session
         let task = session.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 completionHandler(.failure(error))
@@ -137,11 +207,11 @@ extension OpenAI {
     ///   - prompt: Text Prompt
     ///   - model: Defaults to `OpenAIModelType.gpt3(.davinci)` i.e. the most capable model
     ///   - maxTokens: Sets the maximum number of words allowed in the response.
-    ///   - temperature: Adjusts the randomness of the output, which increases from 0 to 1
+    ///   - temperature: Adjusts the randomness of the output, which increases from 0 to 2
     /// - Returns: Returns an OpenAI Data Model
     @available(swift 5.5)
     @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
-    public func sendCompletion(with prompt: String, model: OpenAIModelType = .gpt3(.davinci), maxTokens: Int = 16, temperature: Double = 1) async throws -> OpenAI<TextResult> {
+    public func sendCompletion(with prompt: String, model: OpenAIModelType = .gpt3(.davinci), maxTokens: Int = 16, temperature: Double = 1) async throws -> OpenAIModel<TextResult> {
         return try await withCheckedThrowingContinuation { continuation in
             sendCompletion(with: prompt, model: model, maxTokens: maxTokens, temperature: temperature) { result in
                 continuation.resume(with: result)
@@ -157,7 +227,7 @@ extension OpenAI {
     /// - Returns: Returns an OpenAI Data Model
     @available(swift 5.5)
     @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
-    public func sendEdits(with instruction: String, model: OpenAIModelType = .feature(.davinci), input: String = "") async throws -> OpenAI<TextResult> {
+    public func sendEdits(with instruction: String, model: OpenAIModelType = .feature(.davinci), input: String = "") async throws -> OpenAIModel<TextResult> {
         return try await withCheckedThrowingContinuation { continuation in
             sendEdits(with: instruction, model: model, input: input) { result in
                 continuation.resume(with: result)
@@ -165,17 +235,62 @@ extension OpenAI {
         }
     }
     
-    /// Send Chat request to the OpenAI API
+    /// Send a Chat request to the OpenAI API
     /// - Parameters:
     ///   - messages: Array of `ChatMessages`
     ///   - model: The only support model is `gpt-3.5-turbo`
-    ///   - maxTokens: Sets the maximum number of words allowed in the response.
-    ///   - temperature: AAdjusts the randomness of the output, which increases from 0 to 1
+    ///   - user: An dentifier that represents your end-user (can help OpenAI to monitor and detect abuse).
+    ///   - temperature: Sets the sampling temperature, set between 0 and 2. Higher values will make the output more random, lower values will make it more focused and deterministic. We generally recommend altering this or topProbabilityMass but not both.
+    ///   - topProbabilityMass: The OpenAI api equivalent of the "top_p" parameter. An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or temperature but not both.
+    ///   - choices: How many chat completion choices to generate for each input message.
+    ///   - stop: Up to 4 sequences where the API will stop generating further tokens.
+    ///   - maxTokens: The maximum number of tokens allowed for the generated answer. By default, the number of tokens the model can return will be (4096 - prompt tokens).
+    ///   - presencePenalty: Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    ///   - frequencyPenalty: Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    ///   - logitBias: Modify the likelihood of specified tokens appearing in the completion. Maps tokens (specified by their token ID in the OpenAI Tokenizer—not English words) to an associated bias value from -100 to 100. Values between -1 and 1 should decrease or increase likelihood of selection; values like -100 or 100 should result in a ban or exclusive selection of the relevant token.
+    ///   - completionHandler: Returns an OpenAI Data Model
     @available(swift 5.5)
     @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
-    public func sendChat(with messages: [ChatMessage], model: OpenAIModelType = .chat(.chatgpt), maxTokens: Int? = nil, temperature: Double = 1.0) async throws -> OpenAI<MessageResult> {
+    public func sendChat(with messages: [ChatMessage],
+                         model: OpenAIModelType = .chat(.chatgpt),
+                         user: String? = nil,
+                         temperature: Double? = 1,
+                         topProbabilityMass: Double? = 0,
+                         choices: Int? = 1,
+                         stop: [String]? = nil,
+                         maxTokens: Int? = nil,
+                         presencePenalty: Double? = 0,
+                         frequencyPenalty: Double? = 0,
+                         logitBias: [Int: Double]? = nil) async throws -> OpenAIModel<MessageResult> {
         return try await withCheckedThrowingContinuation { continuation in
-            sendChat(with: messages, model: model, maxTokens: maxTokens, temperature: temperature) { result in
+            sendChat(with: messages,
+                     model: model,
+                     user: user,
+                     temperature: temperature,
+                     topProbabilityMass: topProbabilityMass,
+                     choices: choices,
+                     stop: stop,
+                     maxTokens: maxTokens,
+                     presencePenalty: presencePenalty,
+                     frequencyPenalty: frequencyPenalty,
+                     logitBias: logitBias) { result in
+                continuation.resume(with: result)
+            }
+        }
+    }
+
+    /// Send a Image generation request to the OpenAI API
+    /// - Parameters:
+    ///   - prompt: The Text Prompt
+    ///   - numImages: The number of images to generate, defaults to 1
+    ///   - size: The size of the image, defaults to 1024x1024. There are two other options: 512x512 and 256x256
+    ///   - user: An optional unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.
+    /// - Returns: Returns an OpenAI Data Model
+    @available(swift 5.5)
+    @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+    public func sendImages(with prompt: String, numImages: Int = 1, size: ImageSize = .size1024, user: String? = nil) async throws -> OpenAIModel<UrlResult> {
+        return try await withCheckedThrowingContinuation { continuation in
+            sendImages(with: prompt, numImages: numImages, size: size, user: user) { result in
                 continuation.resume(with: result)
             }
         }
