ce9b12233bf556737064179916e349b9fe458290
Tyler Wysocki <115134475+tywysocki@users.noreply.github.com>
2023-04-18 18:18:59 -0600

    add API for Embeddings

---
+++ Sources/OpenAI/Models/Embeddings.swift
@@ -0,0 +1,13 @@
+//
+//  Embeddings.swift
+//  
+//
+//  Created by Tyler Wysocki  on 2023-04-15.
+//
+
+import Foundation
+
+struct Embeddings: Encodable {
+    let input: String
+    let model: String
+}
